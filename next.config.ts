68b32dc41ed782942db79959c77ef71cd308ff48
Binith Jayasinghe <jpbinith@gmail.com>
2026-01-12 21:25:38 +1100

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    // you can keep this true (no optimizer)
+    unoptimized: true,
+
+    domains: ['health-sight.s3.ap-southeast-2.amazonaws.com'],
+
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'eye-sight.s3.ap-southeast-2.amazonaws.com',
+        hostname: 'health-sight.s3.ap-southeast-2.amazonaws.com',
+        pathname: '/**',
       },
     ],
   },
